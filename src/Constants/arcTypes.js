d08c370bfea2df01df5305b4c777877c25512396
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-06-19 10:06:03 +0200

    Create arcTypes.js

---
+++ src/Constants/arcTypes.js
@@ -0,0 +1,8 @@
+const arcTypes = [
+  {pre: "D", name: "double"}, // double
+  {pre: "O", name: "outer"}, // single outer
+  {pre: "T", name: "triple"}, // triple
+  {pre: "I", name: "inner"}, // single inner
+];
+
+export default arcTypes;
