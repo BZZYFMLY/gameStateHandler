6543a26cda7f85d71c8cff5d3e695d7b96917f78
exphoenee <bozzay.viktor@gmail.com>
2023-06-19 20:58:40 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,17 +1,18 @@
 import RoundHandler from "./RoundHandler/RoundHandler.js";
 import getValidThrowValues from "./Constants/getValidThrowValues.js";
 import createFieldActions from "./UI/createFieldActions.js";
+import UIHandler from "./UI/createFieldActions.js";
 
 
 const roundHandler = new RoundHandler(
   ["Lali", "Mr. Bastardo", "Sebastian", "Mafadaka"],
   2,
-);
+  );
 
-createFieldActions(roundHandler.addThrow.bind(roundHandler));
+const ui = new UIHandler(roundHandler);
 
-const validThrowValues = getValidThrowValues();
-console.log(validThrowValues);
+// const validThrowValues = getValidThrowValues();
+// console.log(validThrowValues);
 
 // const randomThrow = () =>
 //   validThrowValues[Math.floor(Math.random() * validThrowValues.length)];
