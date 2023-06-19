5467ea8f26e60ea95e727af39ab4dc11e0982c1d
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-06-19 10:06:09 +0200

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import RoundHandler from "./RoundHandler/RoundHandler.js";
+import validThrowValues from "./Constants/validThrowValues.js";
+import createDartBoard from "./UI/createDartBoard.js";
+
+createDartBoard();
+
+// const roundHandler = new RoundHandler(
+//   ["Lali", "Mr. Bastardo", "Sebastian", "Mafadaka"],
+//   2,
+// );
+
+// const randomThrow = () =>
+//   validThrowValues[Math.floor(Math.random() * validThrowValues.length)];
+
+// for (let i = 0; i < 50; i++) {
+//   roundHandler.addThrow(randomThrow());
+// }
