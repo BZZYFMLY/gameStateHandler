b70f7ea59df60c3ac0911cc0b0b46bbc4f65becf
exphoenee <bozzay.viktor@gmail.com>
2023-06-19 20:58:45 +0200

    Update createFieldActions.js

--- src/UI/createFieldActions.js
+++ src/UI/createFieldActions.js
@@ -1,45 +1,78 @@
-const createFieldActions = (action) => {
-  const dartBoard = document.getElementById("dartboard");
-  const innerBull = document.getElementById("innerBull");
-  const outerBull = document.getElementById("outerBull");
-  const heatMap = document.getElementById("heatMap");
-  const ctx = heatMap.getContext("2d");
-  const {x, y, width, height} = dartBoard.getBoundingClientRect();
+class UIHandler {
+  constructor(roundHandler) {
+    this.dartBoard = document.getElementById("dartboard");
+    this.innerBull = document.getElementById("innerBull");
+    this.outerBull = document.getElementById("outerBull");
+    this.heatMap = document.getElementById("heatMap");
+    this.ctx = heatMap.getContext("2d");
+    this.roundHandler = roundHandler;
+    this.hitFields = Array.from(
+      this.dartBoard.getElementsByClassName("hit-field"),
+    );
+    this.playerName = document.getElementById("playerName");
+    this.round = document.getElementById("round");
+    this.throws = document.getElementById("throws");
 
-  console.log(x, y, width, height);
+    this.updateGameData();
+    this.createFieldActions();
+  }
 
-  const hitFields = Array.from(dartBoard.getElementsByClassName("hit-field"));
-  [...hitFields, innerBull, outerBull].forEach((hitField) => {
-    hitField.addEventListener("click", (e) => {
-      const field = hitField.getAttribute("data-value");
-      const {clientX, clientY} = e;
-      console.log(clientX - x, clientY - y);
+  playerNameUpdate() {
+    this.playerName.innerHTML = this.roundHandler.currentPlayer;
+  }
 
-      [
-        {radius: 20, opacity: 0.2, color: "orange"},
-        {radius: 7, opacity: 0.1, color: "orange"},
-        {radius: 5, opacity: 0.1, color: "red"},
-      ].forEach(({radius, opacity, color}) => {
-        ctx.save();
-        ctx.beginPath();
-        ctx.fillStyle = color;
-        ctx.save();
-        ctx.globalAlpha = opacity;
-        ctx.arc(
-          clientX - x + 95,
-          clientY - y + 95,
-          radius,
-          radius,
-          0,
-          2 * Math.PI,
-        );
-        ctx.fill();
-        ctx.restore();
+  roundUpdate() {
+    this.round.innerHTML = this.roundHandler.round + 1;
+  }
+
+  throwsUpdate() {
+    this.throws.innerHTML =
+      this.roundHandler.currentPlayerThrows?.join(", ") ?? "";
+  }
+
+  updateGameData() {
+    this.playerNameUpdate();
+    this.roundUpdate();
+    this.throwsUpdate();
+  }
+
+  createFieldActions = () => {
+    [...this.hitFields, this.innerBull, this.outerBull].forEach((hitField) => {
+      hitField.addEventListener("click", (e) => {
+        const fieldName = hitField.getAttribute("data-value");
+
+        const {x, y, width, height} = this.dartBoard.getBoundingClientRect();
+
+        const {clientX, clientY} = e;
+        console.log(clientX - x, clientY - y);
+
+        [
+          {radius: 20, opacity: 0.2, color: "orange"},
+          {radius: 7, opacity: 0.01, color: "orange"},
+          {radius: 5, opacity: 0.01, color: "red"},
+        ].forEach(({radius, opacity, color}) => {
+          this.ctx.save();
+          this.ctx.beginPath();
+          this.ctx.fillStyle = color;
+          this.ctx.save();
+          this.ctx.globalAlpha = opacity;
+          this.ctx.arc(
+            clientX - x + 95,
+            clientY - y + 95,
+            radius,
+            radius,
+            0,
+            2 * Math.PI,
+          );
+          this.ctx.fill();
+          this.ctx.restore();
+        });
+
+        this.roundHandler.addThrow(fieldName);
+        this.updateGameData();
       });
-
-      if (action) action(field);
     });
-  });
-};
+  };
+}
 
-export default createFieldActions;
+export default UIHandler;
